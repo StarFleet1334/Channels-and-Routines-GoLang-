41d4fb9d5887e551697ff7bd9b5df58c3d027866
IliaLataria01 <latariailia6@gmail.com>
2024-02-16 23:03:33 +0400

    Provided code that does website status check,prints which website is on alongside with its statusCode

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://golang.org",
+		"http://amazon.com",
+	}
+
+	for _, link := range links {
+		checkLink(link)
+	}
+}
+
+func checkLink(link string) {
+	ip, err := http.Get(link)
+	if err != nil {
+		fmt.Printf("Error occurred on link: %v", link)
+		return
+	}
+	fmt.Printf("%v is up with statusCode %d\n", link, ip.StatusCode)
 }
