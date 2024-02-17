2c95f814dd05005b9d3cf4104760b108ce135fdd
IliaLataria01 <latariailia6@gmail.com>
2024-02-17 14:27:13 +0400

    Modified the code by Repeating Routines

--- main.go
+++ main.go
@@ -21,8 +21,9 @@ func main() {
 		go checkLink(link, channel)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-channel)
+	for {
+		//fmt.Println(<-channel)
+		checkLink(<-channel, channel)
 	}
 
 }
@@ -31,9 +32,11 @@ func checkLink(link string, channel chan string) {
 	ip, err := http.Get(link) // This is Blocking Call!! So Main Go Routine is suspended
 	if err != nil {
 		fmt.Printf("Error occurred on link: %v", link)
-		channel <- "Error occurred on link: " + link
+		//channel <- "Error occurred on link: " + link.
+		channel <- link
 		return
 	}
 	fmt.Printf("%v is up with statusCode %d\n", link, ip.StatusCode)
-	channel <- "It's up"
+	//channel <- "It's up"
+	channel <- link
 }
